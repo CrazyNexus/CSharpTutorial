fbcae0393c9f93ba49f358df147576f56556dd80
Thomas Dubiel <td@crazynexus.de>
2016-12-11 00:08:23 +0100

    initial commit after lesson 43

---
+++ CSharpTutorial2/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace CSharpTutorial2
+{
+   public class BankAccount
+   {
+      private string name = "Jane";
+      private float balance;
+
+      public float Balance
+      {
+         get { return balance; }
+         set
+         {
+            if (value >= 0)
+            {
+               balance = value;
+            }
+            else
+            {
+               balance = 0;
+            }
+
+         }
+      }
+
+      public BankAccount()
+      {
+         Balance = 22.2f;
+      }
+
+      public float GetCredit(out string name)
+      {
+         name = this.name;
+         return Balance * 5.1f;
+      }
+   }
+}
---
+++ CSharpTutorial2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpTutorial2
+{
+   class MainClass
+   {
+      public static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+
+         BankAccount bankAccount = new BankAccount();
+
+         Console.WriteLine("Initialization: " + bankAccount.Balance);
+
+         bankAccount.Balance = 123.4f;
+         Console.WriteLine("First Assigned Value: " + bankAccount.Balance);
+
+         //bankAccount.Balance = -1234.5f;
+         //Console.WriteLine("Second Assigned Value: " + bankAccount.Balance);
+
+         string username;
+         var credit = bankAccount.GetCredit(out username);
+         Console.WriteLine(username + ", your Credit is: " + credit);
+      }
+   }
+}
