21f2c112068f1114d53744412311a2773841d892
Thomas Dubiel <td@crazynexus.de>
2016-12-12 22:28:31 +0100

    new with generic type as List from System.Collectio.Generic

--- CSharpTutorial2/BankAccount.cs
+++ CSharpTutorial2/BankAccount.cs
@@ -3,7 +3,7 @@ namespace CSharpTutorial2
 {
    public class BankAccount
    {
-      private string name = "Jane";
+      public string name = "Jane";
       private float balance;
 
       public float Balance
@@ -26,6 +26,12 @@ namespace CSharpTutorial2
       public BankAccount()
       {
          Balance = 22.2f;
+      }
+
+      public BankAccount(string name, float balance)
+      {
+         Balance = balance;
+         this.name = name;
       }
 
       // this method returns two values
--- CSharpTutorial2/Program.cs
+++ CSharpTutorial2/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpTutorial2
 {
    class MainClass
    {
+      static List<BankAccount> list;
+
       public static void Main(string[] args)
       {
          Console.WriteLine("Hello World!");
 
-         BankAccount bankAccount = new BankAccount();
+         list = new List<BankAccount>();
+
+         BankAccount bankAccount = new BankAccount("John Doe", 42.88f);
 
          Console.WriteLine("Initialization: " + bankAccount.Balance);
 
-         bankAccount.Balance = 123.4f;
-         Console.WriteLine("First Assigned Value: " + bankAccount.Balance);
+         //bankAccount.Balance = 123.4f;
+         //Console.WriteLine("First Assigned Value: " + bankAccount.Balance);
 
          //bankAccount.Balance = -1234.5f;
          //Console.WriteLine("Second Assigned Value: " + bankAccount.Balance);
@@ -21,6 +26,25 @@ namespace CSharpTutorial2
          string username;
          var credit = bankAccount.GetCredit(out username);
          Console.WriteLine(username + ", your Credit is: " + credit);
+
+         testCollection();
+      }
+
+      static void testCollection()
+      {
+         BankAccount bankAccount = new BankAccount("John Doe", 42.88f);
+         BankAccount bankAccount1 = new BankAccount("Jane Doe", 11.88f);
+         BankAccount bankAccount2 = new BankAccount("Mad Max", 22.88f);
+         BankAccount bankAccount3 = new BankAccount("John Smith", 33.88f);
+         list.Add(bankAccount);
+         list.Add(bankAccount1);
+         list.Add(bankAccount2);
+         list.Add(bankAccount3);
+
+         foreach (var account in list)
+         {
+            Console.WriteLine($"{account.name}: {account.Balance}");
+         }
       }
    }
 }
